b5905c5630b0287beab31fc382efbdf3066e82da
juanmiDAW <juanmiguel.valle@iesdonana.org>
2024-09-24 17:02:49 +0200

    carga inicial

---
+++ servidor/ficha.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Esto es el resultado del formulario </p>
+<?=# "hola"?>
+
+<br>
+<br>
+
+<?php
+    
+    #$x=25;   #la variable x vale 25
+    #$y=30;  /*la varibale y vale 30*/
+    #$z=$x+$y;
+    #echo $z; #echo: expresión evalua y vuelca a la salida el resultado.	
+    #var_dump(3);   #dice el valor y el tipo de dato 
+    #intdiv($z,$y);
+     #echo pow($x, $y);
+
+    /* $x=true;
+     $y=false;*/
+     #$z=$x||$y;
+
+     #$z="25"+"30"."11111";      #(.) concatenacion
+    $z= "hola"+"pepe";
+     var_dump($z)
+    
+?>
+<p>ksknfkdkvkdkdfknlvds</p>
+
+</body>
+</html>
